3199066197e6330c1e2a02add40e91699d0c9e78
J.Johnson <49891794+78raphael@users.noreply.github.com>
2020-05-11 21:54:37 -0700

    W4:  base code

---
+++ Week4/w4.js
@@ -0,0 +1,34 @@
+// Core Requirements
+
+//  01
+//  Build a 3X3 board in HTML and CSS.
+//  You could do this with either a table element, or a collection of divs. Both have their advantages and disadvantages.
+
+//  02
+//  Attach a touchend listener to the board.
+//  When a cell is touched it should add either an 'X or an 'O', depending on whether it is player 1 or player 2's turn.
+
+// 03
+// Add a reset button.
+// When touched the reset button should remove all of the marks from the board.
+
+
+
+
+// Stretch Goals
+
+// 01
+// Borders all around the outside, incorrect
+// Onlt borders on the inside, correct
+// Hint: look into :nth-child(n), :last-child, etc for help to target specific cells to turn borders on or off.
+
+
+// 02
+// Add Game logic
+// Right now the players have to decide who won, or whether it was a tie. Add some game logic to figure out when the game should result in a tie.
+
+
+
+// 03
+// Add More Game logic
+// Add the logic to figure out when someone wins.
