9ef5189f2af7bd453cc2c9ae247202b59633fbff
78raphael <raphael78@gmail.com>
2020-05-19 21:07:25 -0700

    Week 5: WIP

--- Week5/main.js
+++ Week5/main.js
@@ -1,3 +1,16 @@
+import Hikes from './hikes';
+
+const myHike = new Hikes();
+
+
+
+myHike.showHikeList()
+
+
+
+
+
+
 //create an array of hikes
 const hikeList = [
   {
@@ -37,35 +50,7 @@ const hikeList = [
 const imgBasePath = "/Week5/examples/";
 //on load grab the array and insert it into the page
 window.addEventListener("load", () => {
-  showHikeList();
+  hikes.showHikeList();
 });
 
-function showHikeList() {
-  const hikeListElement = document.getElementById("hikes");
-  hikeListElement.innerHTML = "";
-  renderHikeList(hikeList, hikeListElement);
-}
 
-function renderHikeList(hikes, parent) {
-  hikes.forEach(hike => {
-    parent.appendChild(renderOneHike(hike));
-  });
-}
-function renderOneHike(hike) {
-  const item = document.createElement("li");
-
-  item.innerHTML = ` <h2>${hike.name}</h2>
-        <div class="image"><img src="${imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}"></div>
-        <div>
-                <div>
-                    <h3>Distance</h3>
-                    <p>${hike.distance}</p>
-                </div>
-                <div>
-                    <h3>Difficulty</h3>
-                    <p>${hike.difficulty}</p>
-                </div>
-        </div>`;
-
-  return item;
-}
